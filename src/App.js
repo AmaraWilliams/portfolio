fb7fae97b98716b0b1043b9fd4684cb8b1d02e68
AmaraWilliams <amaraajw@gmail.com>
2025-01-30 21:49:01 -0500

    add componets to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
-import navbar from './componets/navbar/navbar'
+import Navbar from './Componets/Navbar/Navbar'
+import React from 'react';
+import headshot from './assets/headshot.JPG'
+import Card from './Componets/Card/Card'
+
+const introContent = () => {
+  return (
+    <div id='intro'>
+      <h1> Hi, I Am 
+        <div id='name'> Amara Williams </div>
+      </h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar/>
       </header>
+      <Card
+        image = {headshot}
+        imgWidth = {400}
+        imgHeight = {400}
+        content = {introContent}
+      />
+      
+        
     </div>
   );
 }
